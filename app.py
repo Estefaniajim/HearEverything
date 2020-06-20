286848993e2aa8df8ff8b5b413b1568d3a55bb3e
Estefania Jimenez Garcia <estefyvocaloid@gmail.com>
2020-06-20 05:15:44 +0000

    command in button

--- app.py
+++ app.py
@@ -6,6 +6,5 @@ Label(root, text = 'Hear Everything', font =(
   'Verdana', 15)).pack(side = TOP, pady = 10) 
 photo = PhotoImage(file = r"mic.png") 
 photoimage = photo.subsample(3, 3) 
-Button(root, text = 'Click Me !', image = photoimage).pack(side = TOP)
-main.wit()
+Button(root, image = photoimage, command = main.wit).pack(side = TOP)
 mainloop() 
