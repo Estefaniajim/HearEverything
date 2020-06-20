9a80656ec117563781ceda5b9fb22462c71746f3
Estefania Jimenez Garcia <estefyvocaloid@gmail.com>
2020-06-20 04:06:30 +0000

    added app

---
+++ app.py
@@ -0,0 +1,9 @@
+from tkinter import * 
+from tkinter.ttk import *
+root = Tk() 
+Label(root, text = 'Hear Everything', font =( 
+  'Verdana', 15)).pack(side = TOP, pady = 10) 
+photo = PhotoImage(file = r"mic.png") 
+photoimage = photo.subsample(3, 3) 
+Button(root, text = 'Click Me !', image = photoimage).pack(side = TOP) 
+mainloop() 
